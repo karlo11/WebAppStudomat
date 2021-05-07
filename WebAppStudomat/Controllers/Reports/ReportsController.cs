84702b245248bcbbf60ee7c765177a598c023ef1
Karlo Ivanović <karlo_115@hotmail.com>
2021-05-07 03:25:00 +0200

    added reports controller

---
+++ WebAppStudomat/Controllers/Reports/ReportsController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Reporting.WebForms;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+
+namespace WebAppStudomat.Controllers.Reports
+{
+    public class ReportsController : Controller
+    {
+        private static readonly string configSSRSUrl = "SSRSReportUrl";
+        private static readonly string configSSRSInitialReport = "SSRSInitialReport";
+
+        // GET: Reports
+        public ActionResult Index()
+        {
+            LoadInitialReport();
+
+            return View();
+        }
+
+        private void LoadInitialReport()
+        {
+            string ssrsUrl = null;
+            string ssrsInitialReport = null;
+            try
+            {
+                ssrsUrl = ConfigurationManager.AppSettings[configSSRSUrl].ToString();
+                ssrsInitialReport = ConfigurationManager.AppSettings[configSSRSInitialReport].ToString();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: ", ex.ToString());
+            }
+
+            var reportViewer = new ReportViewer
+            {
+                ProcessingMode = ProcessingMode.Remote,
+                SizeToReportContent = true,
+                AsyncRendering = true
+            };
+
+            try
+            {
+                reportViewer.ServerReport.ReportServerUrl = new Uri(ssrsUrl);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: ", ex.ToString());
+            }
+
+            reportViewer.ServerReport.ReportPath = ssrsInitialReport;
+            ViewBag.ReportViewer = reportViewer;
+        }
+    }
+}
